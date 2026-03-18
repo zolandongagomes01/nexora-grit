439fae9eda302800153fe84f2fc1ecefb69841bb
Zola Gomes <zolandongagomes19@gmail.com>
2026-03-18 12:33:44 +0100

    fix: middleware export corrected for production build

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(req: NextRequest) {
+export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
 
